3af3e8c8e068bfb9f5a0cc56822cdf46f47dcb88
Alan Liang <alantliang@gmail.com>
2014-09-13 17:42:25 -0400

    Initial commit

---
+++ tract/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tract
+//
+//  Created by Alan Liang on 9/13/14.
+//  Copyright (c) 2014 fsa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
